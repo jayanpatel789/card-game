5bb668d834c7cb6f6b239b2887a0050fde94fae0
jayanpatel789 <86321144+jayanpatel789@users.noreply.github.com>
2024-11-21 21:42:10 +0000

    [UPD] Add shuffle and change initial deck ordering

--- deck.py
+++ deck.py
@@ -1,4 +1,5 @@
 from card import Card
+from random import shuffle
 
 class Deck:
     def __init__(self, include_jokers=False):
@@ -6,11 +7,14 @@ class Deck:
         self.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         self.cards = []
         
-        for rank in self.ranks:
-            for suit in self.suits:
+        for suit in self.suits:
+            for rank in self.ranks:
                 card = Card(rank, suit)
                 self.cards.append(card)
 
         if include_jokers:
-            self.cards.append(Card('Joker', 'Black'))
             self.cards.append(Card('Joker', 'Red'))
+            self.cards.append(Card('Joker', 'Black'))
+
+    def shuffle(self):
+        shuffle(self.cards)
