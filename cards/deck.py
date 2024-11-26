f7400d6d8c84aa18021e6a87714e04b35cd316bc
jayanpatel789 <86321144+jayanpatel789@users.noreply.github.com>
2024-11-26 20:17:28 +0000

    Added string dunder methods for testing

--- cards/deck.py
+++ cards/deck.py
@@ -4,7 +4,8 @@ from random import shuffle
 class Deck:
     def __init__(self, include_jokers=False):
         self.ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
-        self.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        # self.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        self.suits = ['Hearts'] # For testing
         self.cards = []
         
         for suit in self.suits:
@@ -16,6 +17,15 @@ class Deck:
             self.cards.append(Card('Joker', 'Red'))
             self.cards.append(Card('Joker', 'Black'))
 
+    def __str__(self):
+        return f"Deck with {len(self.cards)} cards: {', '.join(str(card) for card in self.cards)}"
+
+    def __repr__(self):
+        return f"Deck({len(self.cards)} cards, top card: {str(self.cards[-1]) if self.cards else 'None'})"
+    
+    def __len__(self):
+        return len(self.cards)
+
     def shuffle(self):
         shuffle(self.cards)
 
@@ -25,3 +35,20 @@ class Deck:
             return card
         else:
             return None
+        
+def test():
+    deck = Deck(include_jokers=True)
+    for _ in range(15):
+        card = deck.draw_card()
+        print(f"Card: {card}")
+    
+    print()
+
+    deck = Deck(include_jokers=True)
+    deck.shuffle()
+    for _ in range(15):
+        card = deck.draw_card()
+        print(f"Card: {card}")
+
+if __name__ == "__main__":
+    test()
