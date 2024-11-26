f33d1c0a78381ce267be15a3000944f120d71fa1
jayanpatel789 <86321144+jayanpatel789@users.noreply.github.com>
2024-11-26 10:52:53 +0000

    Added equality comparisons to Deck and Card

--- cards/card.py
+++ cards/card.py
@@ -4,7 +4,30 @@ class Card:
         self.suit = suit
 
     def __str__(self):
-        return f"{self.rank} of {self.suit}"
+        rank_names = {1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
+        rank_str = rank_names.get(self.rank, self.rank)
+        return f"{rank_str} of {self.suit}"
     
     def __repr__(self):
-        return f"Card('{self.rank}', '{self.suit}')"
+        rank_names = {1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
+        rank_str = rank_names.get(self.rank, self.rank)
+        return f"Card('{rank_str}', '{self.suit}')"
+    
+    def __lt__(self, other):
+        """Less than: Compare if card is less than another card"""
+        if not isinstance(other, Card):
+            return ValueError('Non Card object used in comparison.')
+        return self.rank < other.rank
+    
+    def __gt__(self, other):
+        """Greater than: Compare if card is greater than another card"""
+        if not isinstance(other, Card):
+            return ValueError('Non Card object used in comparison.')
+        return self.rank > other.rank
+    
+    def __eq__(self, other):
+        """Equal to: Compare if card is equal to another card"""
+        if not isinstance(other, Card):
+            return ValueError('Non Card object used in comparison.')
+        return self.rank == other.rank
+        
--- cards/deck.py
+++ cards/deck.py
@@ -3,7 +3,7 @@ from random import shuffle
 
 class Deck:
     def __init__(self, include_jokers=False):
-        self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        self.ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
         self.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         self.cards = []
         
