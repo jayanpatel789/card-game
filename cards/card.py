ec668f0d2d34b0c2bbe6c97d5a3bd158d67efac7
jayanpatel789 <86321144+jayanpatel789@users.noreply.github.com>
2024-11-26 20:16:57 +0000

    Bug fixes to dunder methods

--- cards/card.py
+++ cards/card.py
@@ -4,12 +4,12 @@ class Card:
         self.suit = suit
 
     def __str__(self):
-        rank_names = {1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
+        rank_names = {'1': "Ace", '11': "Jack", '12': "Queen", '13': "King"}
         rank_str = rank_names.get(self.rank, self.rank)
         return f"{rank_str} of {self.suit}"
     
     def __repr__(self):
-        rank_names = {1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
+        rank_names = {'1': "Ace", '11': "Jack", '12': "Queen", '13': "King"}
         rank_str = rank_names.get(self.rank, self.rank)
         return f"Card('{rank_str}', '{self.suit}')"
     
@@ -17,17 +17,32 @@ class Card:
         """Less than: Compare if card is less than another card"""
         if not isinstance(other, Card):
             return ValueError('Non Card object used in comparison.')
-        return self.rank < other.rank
+        return int(self.rank) < int(other.rank)
     
     def __gt__(self, other):
         """Greater than: Compare if card is greater than another card"""
         if not isinstance(other, Card):
             return ValueError('Non Card object used in comparison.')
-        return self.rank > other.rank
+        return int(self.rank) > int(other.rank)
     
     def __eq__(self, other):
         """Equal to: Compare if card is equal to another card"""
         if not isinstance(other, Card):
             return ValueError('Non Card object used in comparison.')
-        return self.rank == other.rank
+        return int(self.rank) == int(other.rank)
+
+
+def test():
+    card0 = Card('4', 'Spades')
+    card1 = Card('6', 'Clubs')
+
+    print(card0 < card1)
+
+    # if '9' > '10':
+    #     print("True")
+    # else:
+    #     print("False")
+
+if __name__ == "__main__":
+    test()
         
