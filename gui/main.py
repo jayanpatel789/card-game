c302c6e83b9743ff15afbf699bd6c8517607da87
jayanpatel789 <86321144+jayanpatel789@users.noreply.github.com>
2024-11-28 19:03:58 +0000

    Moved HomeUI and PlayUI classes to new files

--- gui/main.py
+++ gui/main.py
@@ -1,53 +1,10 @@
-from games.higherOrLower import HigherOrLower
+from gui.homeUI import HomeUI
+from gui.playUI import PlayUI
 from PyQt5.QtWidgets import QMainWindow, QApplication, QStackedWidget, QDialog, QVBoxLayout, QLabel
 from PyQt5.QtGui import QIcon
 from PyQt5.uic import loadUi
 import sys
 import os
-
-
-class HomeUI(QMainWindow):
-    def __init__(self):
-        super(HomeUI, self).__init__()
-        dirpath = os.path.dirname(os.path.abspath(__file__))
-        UIpath = os.path.join(dirpath, r"ui\home_screen.ui")
-        print(UIpath)
-        loadUi(UIpath, self)
-
-        # Connect buttons for navigation
-        self.rulesButton.clicked.connect(self.showRules)
-
-    def showRules(self):
-        # Retrieve the rules from the game instance
-        # Initialise game for rules
-        self.game = HigherOrLower()
-        rules = self.game.getRules()
-
-        # Create a QDialog
-        dialog = QDialog(self)
-        dialog.setWindowTitle("Game Rules")
-        dialog.setGeometry(200, 200, 800, 400)  # Set the size of the dialog
-
-        # Add a QLabel to display the rules
-        layout = QVBoxLayout(dialog)
-        label = QLabel(rules, dialog)
-        label.setWordWrap(True)  # Enable text wrapping
-        layout.addWidget(label)
-
-        # Apply a stylesheet to customize the font
-        label.setStyleSheet("color: white; font-size: 16px; font-family: Arial;")
-
-        dialog.exec_()  # Show the dialog
-
-
-class PlayUI(QMainWindow):
-    def __init__(self):
-        super(PlayUI, self).__init__()
-        dirpath = os.path.dirname(os.path.abspath(__file__))
-        UIpath = os.path.join(dirpath, r"ui\play_screen.ui")
-        print(UIpath)
-        loadUi(UIpath, self)
-
 
 class AppWindow(QMainWindow):
     def __init__(self):
